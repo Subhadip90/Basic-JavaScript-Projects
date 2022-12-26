9b588fad3b70b497109f2c328a6eaa1975a19e1d
Subhadip Chatterjee <50038188+Subhadip90@users.noreply.github.com>
2022-12-26 06:37:56 +0000

    project 11 completed

---
+++ Project 11 - Event Key Codes/script.js
@@ -0,0 +1,20 @@
+const insert = document.getElementById("insert")
+
+window.addEventListener("keydown", (e) => {
+  insert.innerHTML = `
+  <div class="key">
+      ${e.key === '' ? 'Space' : e.key}
+      <small>Key</small>
+    </div>
+
+    <div class="key">
+      ${e.keyCode}
+      <small>Key Code</small>
+    </div>
+
+    <div class="key">
+      ${e.code}
+      <small>Code</small>
+    </div>
+    `
+})
